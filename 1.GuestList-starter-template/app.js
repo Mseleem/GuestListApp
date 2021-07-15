b323c57f6a753832d80c3b243760f4733f4f355d
mseleem <slim.mahmoud@gmail.com>
2021-07-15 23:58:44 +0200

    using v-text and v-html

--- 1.GuestList-starter-template/app.js
+++ 1.GuestList-starter-template/app.js
@@ -4,7 +4,8 @@ new  Vue ({
         event: {
             eventDate: 'August 14th - 16th',
             eventName: 'Summer Festival!',
-            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
+            eventDetails: "Add your name to the guest list for <em>exclusive</em> offers:",
         },
         newNameText: '',
         guestName: [],
